b7ea48d6bc5e55bb7dc8a006cf8f691fffd0d892
aaronjr <aaronjr@users.noreply.github.com>
2021-03-26 11:18:08 +0000

    automated commit by check50 [check50=True]

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+        string name = get_string("What ischeck50 cs50/problems/2021/x/hello your name?\n");
+        printf("hello, %s\n", name);
+}
+
