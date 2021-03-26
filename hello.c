eba98c79211d2808055e69b4c87328e9904be180
aaronjr <aaronjr@users.noreply.github.com>
2021-03-26 11:24:15 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void)
+int main(void)
 {
-        string name = get_string("What is your name?\n");
-        printf("hello, %s\n", name);
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
 }
 
