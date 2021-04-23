d310ddb0f00bafde0e6e1cb3743e8d398aff35dc
aaronjr <aaronjr@users.noreply.github.com>
2021-04-23 10:54:37 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -25,10 +25,10 @@ int main(int argc, string argv[])
     // convert to  ciphertext
     for (int i = 0, n = strlen(plain); i < n; i++){
       if (plain[i] >= 'a' && plain[i] <= 'z'){
-         char c = (plain[i] + key) % 122;
+         char c = (plain[i] + key) % 26;
       printf("%c", c);}
       else if (plain[i] >= 'A' && plain[i] <= 'Z'){
-         char c = (plain[i] + key) % 122;
+         char c = (plain[i] + key) % 26;
       printf("%c", c);}
       else {printf("%c", plain[i]);}
      }
