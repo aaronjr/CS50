1269ada21b2c014e44d8686db04d19e8d29308e2
aaronjr <aaronjr@users.noreply.github.com>
2021-04-24 15:49:11 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -4,11 +4,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
 int main(int argc, string argv[])
 {
-    char key[26];
+   char key[26] = "";
     // check 2 aruguments
    if (argc != 2){printf("Insert valid key\n"); return 1;}
    // check for 26 characters
@@ -16,12 +14,10 @@ int main(int argc, string argv[])
    // check for alphabeetical characters
    for(int i = 0, n = strlen(argv[1]); i < n; i++){
     if ( (argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z')){
-          strncat(key, &argv[1][i], 1);
+         strncat(key, &argv[1][i], 1);
        }
    else {printf("Insert 26 alphabetical characters\n"); return 1;}
    }
-   //string to key
-   
    
    //test for key
    printf("The key is %s\n", key);
@@ -40,7 +36,6 @@ int main(int argc, string argv[])
       printf("%c", c);}
       else {printf("%c", plain[i]);}
      }
-
 
     //print new line
     printf("\n");
