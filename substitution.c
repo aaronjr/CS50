3adabc6dfa864fa825a4ddf03b6b11c10e458dfd
aaronjr <aaronjr@users.noreply.github.com>
2021-04-29 15:58:07 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -19,14 +19,15 @@ int main(int argc, string argv[])
          strncat(key, &argv[1][i], 1);
        }
     else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z'){
-         strncat(key, &argv[1][i], 1);
+         char q = (argv[1][i] + 32);
+         strncat(key, &q , 1);
       }
     
    else {printf("Insert 26 alphabetical characters\n"); return 1;}
    }
 
    //test for key
-   //printf("The key is %s\n", key);
+   printf("The key is %s\n", key);
 
     // get plain text
     string plain = get_string("plaintext: ");
@@ -36,11 +37,9 @@ int main(int argc, string argv[])
     for ( int i = 0, n = strlen(plain); i < n; i++){
       if (plain[i] >= 'a' && plain[i] <= 'z'){
         printf("%c", (key[plain[i] - 'a']));
-       
-        
         }
       else if (plain[i] >= 'A' && plain[i] <= 'Z'){
-        printf("%c", (key[plain[i] - 'A']));
+        printf("%c", (key[plain[i] - 'A'] - 32));
         }
       else {printf("%c", plain[i]);}
     }
