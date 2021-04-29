8d42833c6c8c6a1488879126e091c210a9da2de2
aaronjr <aaronjr@users.noreply.github.com>
2021-04-29 17:31:16 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -8,7 +8,7 @@ int main(int argc, string argv[])
 {
 
    char key[26] = "";
-   
+
     // check 2 aruguments
    if (argc != 2){printf("Insert valid key\n"); return 1;}
    // check for 26 characters
@@ -16,13 +16,27 @@ int main(int argc, string argv[])
    // check for alphabeetical characters
    for(int i = 0, n = strlen(argv[1]); i < n; i++){
     if (argv[1][i] >= 'a' && argv[1][i] <= 'z'){
-         strncat(key, &argv[1][i], 1);
+        for(int f = 0, o = strlen(argv[1]); f < o; f++){
+         for(int j = 0, k = strlen(key); j<k; j++){
+            if(argv[1][f] == key[j]){
+                return 1;
+                printf("No duplicate characters\n");
+            }
+            else {strncat(key, &argv[1][i], 1);}
+        }
+        } 
        }
     else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z'){
-         char q = (argv[1][i] + 32);
-         strncat(key, &q , 1);
-      }
-    
+        for(int m = 0, b = strlen(argv[1]); m <b; m++){
+         for(int j = 0, k = strlen(key); j<k; j++){
+            if(argv[1][m] == key[j]){
+                return 1;
+                printf("No duplicate characters\n");}
+            else {char q = (argv[1][i] + 32);
+                  strncat(key, &q , 1);}
+          }
+        }
+    }
    else {printf("Insert 26 alphabetical characters\n"); return 1;}
    }
 
