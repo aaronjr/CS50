8dfc8729b7b9842e5b1b1e800ded252379bf0c81
aaronjr <aaronjr@users.noreply.github.com>
2021-04-29 17:04:25 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -15,7 +15,7 @@ int main(int argc, string argv[])
    if(strlen(argv[1])!=26){printf("Insert 26 characters\n"); return 1;}
    
    
-   // check for alphabetical characters add to key
+   // check for alphabetical characters & dupllicates and add to key
    for(int i = 0, n = strlen(argv[1]); i < n; i++){
     if (argv[1][i] >= 'a' && argv[1][i] <= 'z'){
         char c = argv[1][i];
@@ -27,8 +27,15 @@ int main(int argc, string argv[])
             else{strncat(key, &argv[1][i], 1);} }
        }
     else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z'){
-         char q = (argv[1][i] + 32);
-         strncat(key, &q , 1);
+         char e = argv[1][i];
+         for(int j = 0, k = strlen(argv[1]); j<k; j++){
+            if(e == argv[1][j]){
+                printf("No duplicate characters\n");
+                return 1;
+            }
+            else{char q = (argv[1][i] + 32);
+                 strncat(key, &q , 1);} }
+         
       }
     
    else {printf("Insert 26 alphabetical characters\n"); return 1;}
