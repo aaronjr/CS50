f34c4573717e4f5f2e4652264fe726485caa7507
aaronjr <aaronjr@users.noreply.github.com>
2021-04-29 16:41:41 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -8,32 +8,33 @@ int main(int argc, string argv[])
 {
 
    char key[26] = "";
-
+   
     // check 2 aruguments
    if (argc != 2){printf("Insert valid key\n"); return 1;}
    // check for 26 characters
    if(strlen(argv[1])!=26){printf("Insert 26 characters\n"); return 1;}
-   
-   //check for duplicate
-   for(int i = 0, n = strlen(argv[1]); i < n; i++){
-       char c = argv[1][i];
+    //check for duplicates
+   for(int d = 0, s = strlen(argv[1]); d < s; d++){
+       char c = argv[1][d];
        for(int j = 0, k = strlen(argv[1]); j<k; j++){
             if(c == argv[1][j]){
-                return 1;
                 printf("No duplicate characters\n");
+                return 1;
+                
             }
+            else {}
        }
    }
-   // check for alphabeetical characters
+   // check for alphabetical characters add to key
    for(int i = 0, n = strlen(argv[1]); i < n; i++){
-    if ((argv[1][i] >= 'a' && argv[1][i] <= 'z')){
+    if (argv[1][i] >= 'a' && argv[1][i] <= 'z'){
          strncat(key, &argv[1][i], 1);
        }
     else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z'){
          char q = (argv[1][i] + 32);
          strncat(key, &q , 1);
       }
-
+    
    else {printf("Insert 26 alphabetical characters\n"); return 1;}
    }
 
@@ -44,13 +45,13 @@ int main(int argc, string argv[])
     string plain = get_string("plaintext: ");
     printf("ciphertext: ");
 
-    // plain text to cipher 
+    // to cipher - CURRENT ISSUE MATH PROBS NOT CORRECT
     for ( int i = 0, n = strlen(plain); i < n; i++){
       if (plain[i] >= 'a' && plain[i] <= 'z'){
         printf("%c", (key[plain[i] - 'a']));
         }
       else if (plain[i] >= 'A' && plain[i] <= 'Z'){
-        printf("%c", (key[plain[i] - 'A'] - 32));
+        printf("%c", (key[plain[i] - 'A']));
         }
       else {printf("%c", plain[i]);}
     }
