f3e9f12ae1ae50489e66db08e8c50e2ee4857fc3
aaronjr <aaronjr@users.noreply.github.com>
2021-04-29 15:54:49 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -19,8 +19,7 @@ int main(int argc, string argv[])
          strncat(key, &argv[1][i], 1);
        }
     else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z'){
-         char q = (argv[1][i] + 32);
-         strncat(key, &q , 1);
+         strncat(key, &argv[1][i], 1);
       }
     
    else {printf("Insert 26 alphabetical characters\n"); return 1;}
