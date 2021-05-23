5f771cebac2926349d427868d6fbc86f6dde1375
aaronjr <aaronjr@users.noreply.github.com>
2021-05-23 22:35:48 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -148,7 +148,6 @@ void tabulate(void)
         for(int j = 0; j < candidate_count; j++)
         if (candidates[preferences[i][j]].eliminated == false){
             candidates[preferences[i][j]].votes++;
-            return;
         }
     }
     return;
