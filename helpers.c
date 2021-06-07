7430c04a8c86a51a46f9c9e014c0a3099dfbeb83
aaronjr <aaronjr@users.noreply.github.com>
2021-06-07 15:29:47 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -68,5 +68,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width / 2; j++){
+            int averageBlue = round((image[i-1][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i][j-1].rgbtBlue + image[i][j].rgbtBlue + image[i][j+1].rgbtBlue + image[i+1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue) / 9);
+            int averageGreen = round((image[i-1][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i][j-1].rgbtGreen + image[i][j].rgbtGreen  + image[i][j+1].rgbtGreen  + image[i+1][j-1].rgbtGreen  + image[i+1][j].rgbtGreen  + image[i+1][j+1].rgbtGreen ) / 9);
+            int averageRed = round((image[i-1][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed + image[i][j-1].rgbtRed + image[i][j].rgbtRed  + image[i][j+1].rgbtRed  + image[i+1][j-1].rgbtRed  + image[i+1][j].rgbtRed  + image[i+1][j+1].rgbtRed ) / 9);
+            
+            image[i][j].rgbtBlue = averageBlue;
+            image[i][j].rgbtGreen = averageGreen;
+            image[i][j].rgbtRed = averageRed;
+        }
+    }
     return;
 }
