9789dfa2abe8b9b7145b42307b7dcca63597692e
aaronjr <aaronjr@users.noreply.github.com>
2021-06-07 11:05:12 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -27,26 +27,28 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float b = image[i][j].rgbtBlue;
             
             image[i][j].rgbtRed = round(0.393 * r + 0.769 * g + 0.189 * b);
-            if(image[i][j].rgbtRed > 255){
+            /*if(image[i][j].rgbtRed > 255){
                 image[i][j].rgbtRed = 255;
             }
             else if(image[i][j].rgbtRed < 0){
                 image[i][j].rgbtRed = 0;
-            }
+            } */
             image[i][j].rgbtGreen = round(0.349 * r + 0.686 * g + 0.168 * b);
-            if(image[i][j].rgbtGreen > 255){
+            /*if(image[i][j].rgbtGreen > 255){
                 image[i][j].rgbtGreen = 255;
             }
             else if(image[i][j].rgbtGreen < 0){
                 image[i][j].rgbtGreen = 0;
-            }
+            } */
             image[i][j].rgbtBlue = round(0.272 * r + 0.534 * g + 0.131 * b);
-            if(image[i][j].rgbtBlue > 255){
+            /*if(image[i][j].rgbtBlue > 255){
                 image[i][j].rgbtBlue = 255;
             }
             else if(image[i][j].rgbtBlue < 0){
                 image[i][j].rgbtBlue = 0;
-            }
+                
+            }*/
+                
         }
     }
     return;
