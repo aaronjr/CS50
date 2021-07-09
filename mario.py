c4b03ad068ab0b8a1344a29c04707c5ef03f127a
aaronjr <aaronjr@users.noreply.github.com>
2021-07-09 12:30:47 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -6,13 +6,13 @@ answer = get_int("Pick a number between 1 - 8: ")
 while answer < 1 or answer > 8:
     print("Number must be between 1 & 8")
     answer = get_int("Pick a number between 1 and 8: ")
-#make pyramid print blank space first, then hash, then gap then hahs
+#make pyramid print blank space first, then hash, then gap then has
 for i in range (answer):
     for j in range (answer - i- 1):
-        print(" ", end=" ")
+        print(" ", end="")
     for h in range (i + 1):
         print("#", end = "")
-    print("  ", end = "")
+    print(" ", end = "")
     for l in range (i + 1):
         print("#", end = "")
     print("")
