8044b68aa475481a3052f80245eb526ffe3a6639
aaronjr <aaronjr@users.noreply.github.com>
2021-07-10 19:52:20 +0000

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -1,22 +1,60 @@
 from cs50 import get_int
 
-cc = get_int("Enter credit card number: ")
-str = str(cc)
-len = len(str)
+def is_valid(cc):
+    strg = str(cc)
+    leng = len(strg)
+    
+    g = ""
+    for i in range (leng -2, -1, -2):
+        multiply = int(strg[i]) * 2
+        g += str(multiply)
+    
+    missed = []
+    for k in range (leng -1, -1, -2):
+        missed.append(int(strg[k]))
+        
+    total = 0
+    for j in range(0,len(g),1):
+        total += int(g[j])
+    
+    p = 0
+    for l in range(0,len(missed),1):
+        p += missed[l]
+        
+    sum = p + total
+    
+    if sum % 10 == 0:
+        return 0
+    else:
+        return 1
+    
+def number(cc):
+    strg = str(cc)
+    leng = len(strg)
+    if leng == 15:
+        if strg[0] == "3" and strg[1] == "4" or strg[1] == "7":
+            print("AMEX")
+    elif leng == 16:
+        if strg[0] == "5" and strg[1] == "1" or strg[1] == "2" or strg[1] == "3" or strg[1] == "4" or strg[1] == "5":
+            print("MASTERCARD")
+        elif strg[0] == "4":
+            print("VISA")
+    elif leng == 13:
+        if strg[0] == "4":
+            print("VISA")
+    else:
+        print("INVALID")
+        
+def main():        
+    
+    cc = get_int("Enter credit card number: ")
+    
+    answer = is_valid(cc)
+    
+    if answer == 0:
+        number(cc)
+    else:
+        print("INVALID")
 
-
-
-
-if len == 15:
-    if str[0] == "3" and str[1] == "4" or str[1] == "7":
-        print("AMEX")
-elif len == 16:
-    if str[0] == "5" and str[1] == "1" or str[1] == "2" or str[1] == "3" or str[1] == "4" or str[1] == "5":
-        print("MASTERCARD")
-    elif str[0] == "4":
-        print("VISA")
-elif len == 13:
-    if str[0] == "4":
-        print("VISA")
-else:
-    print("INVALID")
+if __name__=="__main__":
+    main()
