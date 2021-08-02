0acca1aa9abe2cc020a808702e1e62b40aebad19
aaronjr <aaronjr@users.noreply.github.com>
2021-08-02 09:20:46 +0000

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -5,59 +5,59 @@ from csv import reader, DictReader
 
     
 def main():
-
-
-    # Handle command line arguments and save to pre-variable
+    
+    # Ensure correct usage
     if len(sys.argv) != 3:
-        print("Usage: python dna.py data.csv sequence.txt")
-        exit(1)
-    datab_file = sys.argv[1]
-    sq_file = sys.argv[2]
-    
-    # Open CSV file and save to a dict
-    with open(datab_file, "r") as csv:
-        reader = DictReader(csv)
-        datab= list(reader)
-    # Open sequences file and convert to list
-    with open(sq_file, "r") as file:
-        sq = file.read()
-        
+        sys.exit("Usage example: python dna.py databases/large.csv sequences/5.txt")
 
-    # For each STR, compute longest run of consecutive repeats in      sequence
-    max_counter = []
+    database_file = sys.argv[1]
+    sequence_file = sys.argv[2]
     
+    # open database - save to a list
+    with open(database_file, 'r') as csvfile:
+        reader = DictReader(csvfle)
+        datab = list(reader)
+    
+    # open sequences_file and turn into a list
+    with open(sequence_file, "r") as sq_file:
+        sq = sq_file.read()
+
+    #for each STR find longest repeat
+    max_repetition = []
     for i in range(1, len(reader.fieldnames)):
         s = reader.fieldnames[i]
-        max_counter.append(0)
+        max_repitition.append(0)
         
-    # iterate through the sequence to find s
+        #iterate through the sequence to find s
         for j in range(len(sq)):
             counter = 0
-            # If match found, counter += 1
+            #if match cound counter plus 1 
             if sq[j:j + len(s)] == s:
-                k = 0
-                while sq[j + k:j + k + len(s)] == s:
-                    counter += 1
-                    k += len(s)
-                # if there is a  new maximum of repeats - update max_counter
-                if counter > max_counter[i - 1]:
-                    max_counter[i - 1] = counter
-
-    # Compare against data
+                k=0
+                while sq[j + k : j + k + len(s) == s]:
+                    counter+= 1
+                    ke += len(S)
+                #if new maximum of repitions - update max_repitions
+                if counter > max_repitions[i - 1]:
+                    max_repitions[i - 1] = counter
+   
+    #compare against data
     for i in range(len(datab)):
         matches = 0
         for j in range(1, len(reader.fieldnames)):
-            if int(max_counter[j - 1]) == int(datab[i]  [reader.fieldnames[j]]):
+            if int(max_repititions[j-1]) == int(datab[i] [reader.fieldnames[j]])
                 matches += 1
             if matches == (len(reader.fieldnames) - 1):
-                print(datab[i]['name'])
-                exit(0)
-    
+                print(datab[i]["name"])
+                
     print("No match")
-
+    
     
 if __name__ == "__main__":
     main()
 
+
 #print(datab[0]['name'])
     #print(sequence)
+    
+    
