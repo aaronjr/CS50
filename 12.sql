485387440f1bbe99a30fd550ee92849b5525b2e2
aaronjr <aaronjr@users.noreply.github.com>
2021-08-24 11:12:53 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name = 'Johnny Depp' OR 'Helena Bonham Carter';
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name = 'Johnny Depp' AND 'Helena Bonham Carter';
