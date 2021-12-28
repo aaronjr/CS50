76fffd603fbd3da3969286b6f439f062a7a4ce16
aaronjr <aaronjr@users.noreply.github.com>
2021-12-28 17:05:06 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -22,45 +22,37 @@ def main():
     with open(sequence_file, "r") as sq_file:
         sq = sq_file.read()
 
-    #for each STR find longest repeat
+    # for each STR find longest repeat
     max_repetition = []
     for i in range(1, len(reader.fieldnames)):
         s = reader.fieldnames[i]
         max_repetition.append(0)
         
-        #iterate through the sequence to find s
+        # iterate through the sequence to find s
         for j in range(len(sq)):
             counter = 0
-            #if match cound counter plus 1 
+            # if match cound counter plus 1 
             if sq[j:j + len(s)] == s:
-                k=0
-                while sq[j + k : j + k + len(s)] == s:
+                k = 0
+                while sq[j + k: j + k + len(s)] == s:
                     counter += 1
                     k += len(s)
-                #if new maximum of repitions - update max_repitions
+                # if new maximum of repitions - update max_repitions
                 if counter > max_repetition[i - 1]:
-                   max_repetition[i - 1] = counter
+                    max_repetition[i - 1] = counter
    
-    #compare against data
+    # compare against data
     for i in range(len(datab)):
         matches = 0
         for j in range(1, len(reader.fieldnames)):
-            if int(max_repetition[j-1]) == int(datab[i] [reader.fieldnames[j]]):
+            if int(max_repetition[j-1]) == int(datab[i][reader.fieldnames[j]]):
                 matches += 1
             if matches == (len(reader.fieldnames) - 1):
                 print(datab[i]['name'])
                 exit(0)
    
-    
     print("No match")   
                 
     
-    
 if __name__ == "__main__":
     main()
-
-
-#print(datab[0]['name'])
-    #print(sequence)
-    
-    
