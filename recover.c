20884c4979159997a81b7d2e4c05a9e4a01ec5df
aaronjr <aaronjr@users.noreply.github.com>
2021-12-28 16:17:07 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -7,7 +7,8 @@ typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
     
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("Usage: ./recover card.raw\n");
         return 1;
     }
@@ -29,32 +30,31 @@ int main(int argc, char *argv[])
    
     
     //loop through file - 512 bytes at a time store in buffer
-    while(fread(buffer, sizeof(char), 512, file) != 0)
+    while (fread(buffer, sizeof(char), 512, file) != 0)
     {
         //check first 4 bytes
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) 
-         {
-           //make name
-           sprintf(filename, "%03i.jpg", counter);
-           //open to write
-           output = fopen(filename, "w");
-           //write to file
-           fwrite(buffer, sizeof(buffer), 1, output);
-           //counter++
-           counter++;
-         }
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) 
+        {
+            //make name
+            sprintf(filename, "%03i.jpg", counter);
+            //open to write
+            output = fopen(filename, "w");
+            //write to file
+            fwrite(buffer, sizeof(buffer), 1, output);
+            //counter++
+            counter++;
+        }
            
         else if (output != NULL)
         {
-         fwrite(buffer, sizeof(buffer), 1, output);
+            fwrite(buffer, sizeof(buffer), 1, output);
         }
     }
     
       
 
  
- fclose(file);
- fclose(output);
+    fclose(file, output);
  
- return 0;
+    return 0;
 }
