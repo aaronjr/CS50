7dbc3d49881ff535c970d8d133ef32e90a02ed04
aaronjr <aaronjr@users.noreply.github.com>
2021-12-28 14:59:16 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -4,35 +4,40 @@
 
 int main(void)
 {
-//get input
-int input;
-
-do  {
-    input = get_int("height of tower: ");
-    }
-while (input > 8 || input < 1);
-
-// work out
-
-for (int i = 0; i<input; i++){
-    for (int j = input - 1; j > i; j--)
+    //get input
+    int input;
+    
+    do
     {
-        printf(" ");
+        input = get_int("height of tower: ");
     }
-    for (int h = -1; h < i; h++)
+    while (input > 8 || input < 1);
+    
+    // work out
+    for (int i = 0; i < input; i++)
     {
-        printf("#");
+        for (int j = input - 1; j > i; j--)
+        {
+            // print empty space
+            printf(" ");
+        }
+        for (int h = -1; h < i; h++)
+        {
+            // print hash
+            printf("#");
+        }
+        printf("  ");
+        for (int k = i - 1; k > i; k--)
+        {
+            // print empty space
+            printf(" ");
+        }
+        for (int l = -1; l < i; l++)
+        {   
+            // print another hash
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("  ");
-    for (int k = i - 1; k > i; k--)
-    {
-        printf(" ");
-    }
-     for (int l = -1; l < i; l++)
-    {
-        printf("#");
-    }
-    printf("\n");
-}
 
 }
